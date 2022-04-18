7a96c38e5f204839872913c56f153c9d86f5a822
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-04-18 18:20:40 +0900

    for문

---
+++ src/main/java/Control/For.java
@@ -0,0 +1,38 @@
+package Control;
+import java.lang.String;
+
+public class For {
+    public static void main(String[] args) {
+        String[] numbers = {"one", "two", "three"};
+        for(int i=0; i<numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        int[] marks_1 = {90, 25, 67, 45, 80};
+        for(int i=0; i<marks_1.length; i++) {
+            if (marks_1[i] >= 60) {
+                System.out.println((i+1)+"번 학생은 합격입니다.");
+            }
+            else {
+                System.out.println((i+1)+"번 학생은 불합격입니다.");
+            }
+        }
+        // for 와 continue
+        int[] marks_2 = {90, 25, 67, 45, 80};
+        for(int i=0; i<marks_2.length; i++) {
+            if (marks_2[i] < 60) {
+                continue;
+            }
+            System.out.println((i+1)+"번 학생 축하합니다. 합격입니다.");
+        }
+
+        // 이중 for 문
+        for(int i=2; i<10; i++) {
+            for(int j=1; j<10; j++) {
+                System.out.print(i*j+" ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
