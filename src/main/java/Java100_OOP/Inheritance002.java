728a98e4aa7b4b8027dde221be7d8a436f5a0546
jiwon5304 <80395324+jiwon5304@users.noreply.github.com>
2022-04-21 18:20:04 +0900

    OOP_3

--- src/main/java/Java100_OOP/Inheritance002.java
+++ src/main/java/Java100_OOP/Inheritance002.java
@@ -1,80 +1,148 @@
-// 클래스의 상속을 코드로 구현해보시오.
-// 이 문제는 자바 OOP 문법에서 상속에 대한 개념과 그것을 코드로 구현할 수 있는지를 묻는 문제이다.
+// getter, setter가 포함된 클래스의 상속을 코드로 구현해보시오.
+// 이 문제는 클래스의 상속을 구현시 getter, setter의 개념과 용도를 알고 있는지를 묻는 문제이다.
+// 부모 클래스 --> Person, 자식 클래스 --> Villain, Hero
 
 package Java100_OOP;
 
-class Person_3 {
-    // Field(변수)
-    int gender;
-    int power;
+class Person_4 {
+    // Field
+    private String name;                    // private: 외부에서 변수값을 마음대로 변경할 수 없도록 하기 위해
+    private int age;
+    private int height;
+    private int weight;
 
-    // Constructor(생성자)
-    Person_3() {
-        this.gender = 1;		// 1: 남성, 2: 여성
-        this.power = 100;	    // 기본파워
+    // Constructor
+    Person_4() {}
+    Person_4 (String name, int age, int height, int weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
     }
 
     // Method
-    void walk() {
-        System.out.println( "걸어가고 있어요~" );
+    public String getName() { return name; }                            // 값을 가져오기
+    public void setName( String name ) { this.name = name; }            // 값을 세팅, this: p1
+
+    public int getAge() { return age; }
+    public void setAge( int age ) { this.age = age; }
+
+    public int getHeight() { return height; }
+    public void setHeight( int height ) { this.height = height; }
+
+    public int getWeight() { return weight; }
+    public void setWeight( int weight ) { this.weight = weight; }
+
+    public void move() {
+        System.out.println("이동중...");
     }
 }
 
-// Person_3 부모 클래스를 상속받아 Hero 자식 클래스 생성
-class Hero extends Person_3 {
+class Villain_2 extends Person_4 {
     // Field
-    String name;
-    int age;
+    private String unique_key;
+    private int weapon;         // 1~9 숫자로 무기 분류: 1(창), 2(방패), 3(총)
+    private double power;
 
     // Constructor
-    Hero() {}           // 빈 생성자 생성
-    Hero( String name, int age ) {
-        super();            // 부모 클래스의 생성자 호출, 생략을 하더라도 컴파일러가 자동으로 호출
-        this.name = name;
-        this.age = age;
+    Villain_2() {
     }
 
-    // Method : 부모 클래스 Method 오버라이딩(!오버로딩과는 다름)
-     void walk() {
-     System.out.println( "2배로 빨리 걸어가고 있어요~" );
-     }
-
-    void eat() {
-        System.out.println( "밥먹고 있어요~" );
+    Villain_2(String name, int age, int height, int weight, String unique_key, int weapon, double power) {
+        super(name, age, height, weight);     // 부모클래스 생성자 호출
+        this.unique_key = unique_key;
+        this.weapon = weapon;
+        this.power = power;
     }
 
-    void displayPerson() {
-        System.out.println( "이름 : " + name  + ", 나이 : " + age + ", 성별 : " + gender + ", 파워 : " + power );
+    // Method
+    public String getUnique_key() {
+        return unique_key;
+    }
+
+    public void setUnique_key(String unique_key) {
+        this.unique_key = unique_key;
+    }
+
+    public int getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(int weapon) {
+        this.weapon = weapon;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    // printPerson()
+    public void printPerson() {
+        System.out.println("---------------------------------");
+        System.out.println("악당 이름 : " + getName());
+        System.out.println("악당 나이 : " + getAge());
+        System.out.println("악당의 키 : " + getHeight());
+        System.out.println("악당 체중 : " + getWeight());
+        System.out.println("악당 넘버 : " + getUnique_key());
+        System.out.println( "악당 무기 : " + getWeaponName( getWeapon() ) ); // 숫자(1~9) --> 1: 창, 2: 방패, 3: 총..
+        System.out.println("악당 파워 : " + getPower());
+        System.out.println("---------------------------------");
+
+    }
+
+    // getWeaponName()
+    public String getWeaponName( int a ) {
+        String weapon;
+        switch( a ) {
+            case 1:
+                weapon = "창";
+                break;
+            case 2:
+                weapon = "방패";
+                break;
+            case 3:
+                weapon = "총";
+                break;
+            default:
+                weapon = "---";
+                break;
+        }
+        return weapon;
     }
 }
 
-class Villain {}
+class Hero_2 extends Person_4 {}
 
 public class Inheritance002 {
     public static void main(String[] args) {
 
         // 1. 객체 생성
-        Person_3 p1 = new Person_3();
-        p1.walk();
-        // p1.eat();        // Hero에 있는 메소드이므로 에러
+        Person_4 p1 = new Person_4();
 
-        // 2. 상속을 통한 슈퍼맨 객체 생성
-        // 1) gender, power 가 먼저 초기값세팅 2)name, age 초기값 세팅
-        Hero h1 = new Hero( "슈퍼맨", 20 );
-        System.out.println( h1.name );
-        System.out.println( h1.age );
-        System.out.println( h1.gender );	// 부모 클래스 필드(변수)
-        System.out.println( h1.power );	    // 부모 클래스 필드(변수)
+        // 2. 메소드 호출 : setName 이라는 메소드에 "홍길동" 이라는 값을 넣어서 호출
+        p1.setName("홍길동");                    //  p1.name = "..." : private 이기 때문에 에러발생
+        System.out.println(p1.getName());
 
-        // 메소드 오버라이딩: 부모 클래스에서 상속받았지만 자식자식 클래스에서 오버라이딩
-        h1.walk();              //2배로 빨리 걸어가고 있어요~
+        // 1. 객체 생성
+        Villain_2 v1 = new Villain_2( "좀비", 20, 180, 80, "15001231", 2, 99.5 );
+        v1.printPerson();
+        System.out.print( v1.getName() + " " );
+        v1.move();  // 부모 클래스의 move() 메서드를 호출.
 
-        // 3. 원더우먼 객체 생성
-        Hero h2 = new Hero( "원더우먼", 30);
-        h2.displayPerson();
-        h2.gender = 2;
-        h2.power = 300;
-        h2.displayPerson();
-        h2.walk();
+        // 1. 객체 생성
+        Villain_2 v2 = new Villain_2( "도깨비", 30, 175, 70, "11001121", 1, 77.5 );
+        v2.printPerson();
+        System.out.print( v2.getName() + " " );
+        v2.move();
+
+        // 1. 객체 생성
+        Villain_2 v3 = new Villain_2( "몽달귀신", 40, 150, 40, "10001010", 3, 27.5 );
+        v3.printPerson();
+        System.out.print( v3.getName() + " " );
+        v3.move();
     }
 }
